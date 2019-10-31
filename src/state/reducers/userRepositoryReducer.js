ae6dea76d56cfb22fd6e5b6a3254aa29c787ec8a
Kevin Picasco <kevin.picasco@gmail.com>
2019-10-31 10:59:50 +0800

    Update UserRepositoryReducer State Management
    Update initial state
    Reset/update state based on action type

--- src/state/reducers/userRepositoryReducer.js
+++ src/state/reducers/userRepositoryReducer.js
@@ -1,13 +1,13 @@
 import { GET_USER_REPOSITORIES, GET_USER_REPOSITORIES_SUCCESS, GET_USER_REPOSITORIES_FAILED } from '../actions/userRepositoryAction';
 
-export default (state = { isLoading: false, data: [] }, action) => {
+export default (state = { isLoading: false, data: null, error: null }, action) => {
     switch (action.type) {
         case GET_USER_REPOSITORIES:
             return { ...state, isLoading: true };
         case GET_USER_REPOSITORIES_SUCCESS:
-            return { ...state, data: action.data, isLoading: false };
+            return { ...state, data: action.data, error: null, isLoading: false };
         case GET_USER_REPOSITORIES_FAILED:
-            return { ...state, error: action.error, isLoading: false };
+            return { ...state, data: null, error: action.error, isLoading: false };
         default:
             return state;
     }
