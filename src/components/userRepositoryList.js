d1a9a885a0928e3337e14e143bd33980f519407e
Kevin Picasco <kevin.picasco@gmail.com>
2019-10-31 18:13:04 +0800

    Update userRepositoryList Style
    Prettify userRepository styles

--- src/components/userRepositoryList.js
+++ src/components/userRepositoryList.js
@@ -2,27 +2,83 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getUserRepositories } from '../state/actions/userRepositoryAction';
 import Moment from 'react-moment';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Icon from '@material-ui/core/Icon';
+
+const useStyles = makeStyles(theme => ({
+    rootContainer: {
+        margin: theme.spacing(5)
+    },
+    root: {
+        flexGrow: 1,
+        textAlign: 'left',
+        borderBottomWidth: 1,
+        borderBottomColor: '#cfcfcf',
+        borderBottomStyle: 'solid',
+        marginTop: theme.spacing(2),
+        paddingBottom: theme.spacing(2)
+    },
+    desc: {
+        color: '#586069',
+        paddingTop: '5px'
+    },
+    tag: {
+        color: '#586069',
+        fontSize: '12px'
+    }
+}));
 
 const UserRepository = ({ userRepository }) => {
+    const [spacing] = React.useState(2);
+    const classes = useStyles();
+
     return (
-        <div>
-            {userRepository.name}
-            <br />
-            <Moment format="D MMM YYYY hh:mm A">{userRepository.created_at}</Moment>
-            <br />
-            <a
-                className="App-link"
-                href={userRepository.html_url}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                {userRepository.html_url}
-            </a>
-        </div>
+        <Grid container className={classes.root} spacing={0}>
+            <Grid item xs={12}>
+                <h3>
+                    <a
+                        className="App-link"
+                        href={userRepository.html_url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {userRepository.name}
+                    </a>
+                </h3>
+            </Grid>
+            <Grid item xs={12} className={classes.desc}>
+                {userRepository.description}
+            </Grid>
+            <Grid item xs={12} style={{ paddingTop: '8px' }}>
+                <Grid container spacing={spacing}>
+                    {
+                        userRepository.language &&
+                        <Grid item className={classes.tag} title="Language">
+                            <Icon className="fa fa-circle" style={{ fontSize: '12px', width: 'auto' }} /> {userRepository.language}
+                        </Grid>
+                    }
+                    <Grid item className={classes.tag} title="Fork">
+                        <Icon className="fa fa-code-branch" style={{ fontSize: '12px', width: 'auto' }} /> {userRepository.forks}
+                    </Grid>
+                    <Grid item className={classes.tag} title="Watch">
+                        <Icon className="fa fa-eye" style={{ fontSize: '12px', width: 'auto' }} /> {userRepository.watchers}
+                    </Grid>
+                    <Grid item className={classes.tag} title="Issue">
+                        <Icon className="fa fa-exclamation-circle" style={{ fontSize: '12px', width: 'auto' }} /> {userRepository.open_issues}
+                    </Grid>
+                    <Grid item className={classes.tag}>
+                        Created on <Moment format="MMM D, YYYY hh:mm A">{userRepository.created_at}</Moment>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
 let UserRepositoryList = ({ filter, userRepositories, getUserRepositories }) => {
+    const classes = useStyles();
+
     useEffect(() => {
         getUserRepositories(filter);
     }, [filter]);
@@ -35,8 +91,12 @@ let UserRepositoryList = ({ filter, userRepositories, getUserRepositories }) => 
                     : userRepositories.error
                         ? `${userRepositories.error}`
                         : userRepositories.data && userRepositories.data.length
-                            ? userRepositories.data.map(userRepository =>
-                                <UserRepository key={userRepository.id} userRepository={userRepository}></UserRepository>)
+                            ? <div className={classes.rootContainer}>
+                                {
+                                    userRepositories.data.map(userRepository =>
+                                        <UserRepository key={userRepository.id} userRepository={userRepository}></UserRepository>)
+                                }
+                            </div>
                             : 'User has no repository'
             }
         </div >
