ac38e7dde1170fe84ea72c4bbd027b2280783ba3
Kevin Picasco <kevin.picasco@gmail.com>
2019-10-31 11:02:22 +0800

    Validate userRepositorySaga
    Validate request
    Check and call appropriate action based on response

--- src/state/sagas/userRepositorySaga.js
+++ src/state/sagas/userRepositorySaga.js
@@ -1,14 +1,40 @@
 import { put, takeEvery } from 'redux-saga/effects';
 import { GET_USER_REPOSITORIES, getUserRepositoriesSuccess, getUserRepositoriesFailed } from '../actions/userRepositoryAction';
+import * as Constant from '../../utils/constant';
+
+const defaultErrorMessage = 'User not found';
 
 function* getUserRepositories(action) {
     try {
-        const resJson = yield fetch(`https://api.github.com/users/${action.filter.username}/repos`)
+        // Validation
+        if (!action || !action.filter || !action.filter.username || !action.filter.username || action.filter.username.length == 0) {
+            yield put(getUserRepositoriesFailed('Username is empty'));
+            return;
+        }
+
+        let url = `${Constant.GITHUB_API_URL}/users/${action.filter.username}/repos?`;
+
+        if (action.filter.sort && action.filter.sort.length > 0) {
+            url += `sort=${action.filter.sort}`;
+        }
+
+        if (action.filter.isDescDirection != null) {
+            url += `direction=${action.filter.isDescDirection ? Constant.GITHUB_API_SORT_DIRECTION_DESC : Constant.GITHUB_API_SORT_DIRECTION_ASC}`;
+        }
+
+        const resJson = yield fetch(url)
             .then(res => res.json());
 
-        yield put(getUserRepositoriesSuccess(resJson));
+        if (Array.isArray(resJson)) {
+            yield put(getUserRepositoriesSuccess(resJson));
+        } else if (resJson.message && resJson.message.length && resJson.message.length > 0) {
+            yield put(getUserRepositoriesFailed(resJson.message));
+        } else {
+            yield put(getUserRepositoriesFailed(defaultErrorMessage));
+        }
     } catch (err) {
-        yield put(getUserRepositoriesFailed(err));
+        // Hide actual err from end-user
+        yield put(getUserRepositoriesFailed(defaultErrorMessage));
     }
 }
 
