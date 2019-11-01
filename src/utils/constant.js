a18926955f44db9d6244f3a48d469a56fb42b52d
Kevin Picasco <kevin.picasco@gmail.com>
2019-11-01 14:52:50 +0800

    Add Constant String Template
    Include string templates that are commonly shared in the app

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,6 @@
 export const GITHUB_API_URL = 'https://api.github.com';
 export const GITHUB_API_SORT_DIRECTION_ASC = 'asc';
 export const GITHUB_API_SORT_DIRECTION_DESC = 'desc';
+
+export const MESSAGE_NOT_FOUND = '{0} not found';
+export const MESSAGE_EMPTY = '{0} is empty';
